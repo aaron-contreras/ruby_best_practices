df5c26dbaba6a150417800409969c7e456a7b9e8
Aaron Contreras <aaronlcaq@gmail.com>
2020-10-07 12:42:29 -0500

    Add questioner code

---
+++ chapter_1/advanced_testing_techniques/lib/questioner.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Questioner
+  def ask(question)
+    puts question
+    response = gets.chomp
+    case(response)
+    when /^y(es)?$/i
+      true
+    when /^no?$/i
+      false
+    else
+      puts "I don't understand."
+      ask question
+    end
+  end
+end
+
