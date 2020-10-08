3939e0c52fb65dee49a53587bdff3dbe4a3be62d
Aaron Contreras <aaronlcaq@gmail.com>
2020-10-08 15:52:57 -0500

    Add pseudo-keyword arguments

--- chapter2/flexible_argument_processing/example_args.rb
+++ chapter2/flexible_argument_processing/example_args.rb
@@ -10,3 +10,17 @@ def load_file2(name='example_args.rb', mode='rb')
   File.open(name, mode)
 end
 
+# Pseudo-keyword arguments
+def story(options)
+  "#{options[:person]} went to town, riding on a #{options[:animal]}"
+end
+
+def story_improved(options={})
+  options = { person: 'Yankee Doodle', animal: 'Tiger' }.merge(options)
+  "#{options[:person]} went to to town, riding on a #{options[:animal]}"
+end
+
+def write_story_to_file(file, options={})
+  File.open(file, 'w') { |f| f << story_improved(options) }
+end
+
