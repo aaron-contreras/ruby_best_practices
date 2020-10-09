adedd924898f57d9cf228d1575eff952ff8604f4
Aaron Contreras <aaronlcaq@gmail.com>
2020-10-09 17:47:20 -0500

    Add array splat operator code

--- chapter2/flexible_argument_processing/example_args.rb
+++ chapter2/flexible_argument_processing/example_args.rb
@@ -2,6 +2,32 @@ def distance(x1, y1, x2, y2)
   Math.hypot(x2 - x1, y2 - y1)
 end
 
+def distance2(*points)
+  distance(*points.flatten)
+end
+
+def distance3(*points)
+  case(points.length)
+  when 2
+    x1, y1, x2, y2 = points.flatten
+  when 4
+    x1, y1, x2, y2 = points
+  else
+    raise ArgumentError,
+      'Points may be specified as [x1, y1], [x2, y2] or x1, y1, x2, y2'
+  end
+
+  Math.hypot(x2 - x1, y2 - y1)
+end
+
+
+# Using #is_a? instead of === based on style guide
+def distance4(*points)
+  x1, y1, x2, y2 = points.flatten
+  raise ArgumentError unless [x1, y1, x2, y2].all? { |e| e.is_a? Numeric }
+  Math.hypot(x2 - x1, y2 - y1)
+end
+
 def load_file(name, mode='rb')
   File.open(name, mode)
 end
